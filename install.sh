7df1c40aec880791a7755cfcb05baab776d0ad4d
bartTam <mrbart.tam@gmail.com>
2018-04-17 13:15:09 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,3 @@
-#!/bin/sh
-
-sudo apt-get update
-yes | sudo apt-get upgrade
-yes | sudo apt-get install python3-pip git
-
-sudo pip3 install --upgrade pip
-sudo pip3 install Flask
-
 #!/bin/sh                                                                                                
                                                                                                          
 sudo apt-get update                                                                                      
